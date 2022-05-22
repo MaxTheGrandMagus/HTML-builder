24fc69ef0053696b6cb41d232e17cb72744f8b6d
Max <veyronmclord@gmail.com>
2022-05-23 05:31:29 +0600

    feat: add task03 solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, './secret-folder'), { withFileTypes: true }, (err, files) => {
+  if (err) {
+    throw err;
+  }
+  files.forEach((file) => {
+    if (file.isFile()) {
+      let fileName = file.name.split('.')[0];
+      let fileExtension = path.extname(file.name).split('.')[1];
+      fs.stat(path.join(__dirname, './secret-folder', file.name), (err, stats) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log(`${fileName} - ${fileExtension} - ${stats.size / 1024} KB`);
+        }
+      });
+    }
+  });
+});
